c21c863cb3a10605a04205555800944bc04c0500
Harsh Kasat <hkasat@waymore.io>
2024-06-24 22:14:57 +0530

    added cd/ ci pipeline

--- LLM/Config/llm_config.py
+++ LLM/Config/llm_config.py
@@ -8,7 +8,8 @@ class ConfigLLM():
 
     def __init__(self):
         try:
-            self.gemini_api_key = os.getenv('GEMINI_API_KEY')
+            # self.gemini_api_key = os.getenv('GEMINI_API_KEY')
+            self.gemini_api_key = "AIzaSyCn65BTEOXQSASU111_j0chBZtY8le4laM"
 
             if self.gemini_api_key is None:
                 raise ValueError ("Gemini API key is not given")
