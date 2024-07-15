f28cdd1024985d1c92b16af79c030cdfd595bc90
Harsh Kasat <hkasat@waymore.io>
2024-07-16 00:35:34 +0530

    reducing limit per interval to 4

--- Redis/LimitingAlgo/limiting_algo.py
+++ Redis/LimitingAlgo/limiting_algo.py
@@ -12,7 +12,7 @@ class RateLimiter:
 
     def __init__(self):
         self.interval = 86400 # 24 hours
-        self.limit_per_interval = 1000 # thousands requests per 24 hour interval
+        self.limit_per_interval = 4 # four requests per 24 hour interval
         self.lock = threading.Lock()
         self.cache = Cache()
 
