5f53b77ac06e59108dd38fb228b67b1638d07890
Harsh Kasat <hkasat@waymore.io>
2024-07-17 05:05:40 +0530

    removing safe measure

--- LLM/Config/llm_config.py
+++ LLM/Config/llm_config.py
@@ -4,6 +4,25 @@ import google.generativeai as genai
 
 load_dotenv()
 
+safe = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
+
 class ConfigLLM():
 
     def __init__(self):
@@ -20,7 +39,7 @@ class ConfigLLM():
         try:
             genai.configure(api_key = self.gemini_api_key)
 
-            self.gemini_pro_vision_models = genai.GenerativeModel('models/gemini-pro-vision')
+            self.gemini_pro_vision_models = genai.GenerativeModel('models/gemini-pro-vision', safety_settings=safe)
         except Exception as e:
             print(f'When trying to configure the gemini pro vision model error found: {e}')
 
